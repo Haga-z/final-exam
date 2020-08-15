c154c29fe4206164cf12b7dc49f89c2129d6998c
Haga <arstan014@gmail.com>
2020-08-15 19:00:46 +0600

    Insert user

--- src/main/java/kg/places/reviews/exam/config/SecurityConfig.java
+++ src/main/java/kg/places/reviews/exam/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 
     @Override
---
+++ src/main/resources/db/migration/V4__insert_tables.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(id,email,password,fullname,enabled)values
+(1,'admin@gmail.com','$2y$12$xrXCPczaMUTrosaynxIKLOo6qMWXx8W5yJ5X/0oZE6sAYWxKjF/uG','admin',1);
