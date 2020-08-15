3c62d2ec845462168708ba83308f4415c193c9c2
Haga <arstan014@gmail.com>
2020-08-15 15:12:51 +0600

    Created review

---
+++ src/main/java/kg/places/reviews/exam/model/Review.java
@@ -0,0 +1,33 @@
+package kg.places.reviews.exam.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor
+@Entity
+@ToString
+@Table(name = "places")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String text;
+
+    private int mark;
+
+    private Timestamp date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Place place;
+}
---
+++ src/main/resources/db/migration/V3__create_review_table.sql
@@ -0,0 +1,14 @@
+use `exam`;
+
+CREATE TABLE IF NOT EXISTS `reviews`(
+    `id` int auto_increment NOT NULL,
+    `text` varchar(500) NOT NULL,
+    'mark' int not null ,
+    `user_id` int NOT NULL ,
+    `place_id` int NOT NULL ,
+    PRIMARY KEY (`id`),
+    foreign key (user_id)references `users`(id),
+    foreign key (place_id)references `places`(id)
+);
+
+
